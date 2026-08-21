5488b78cafe453e48fe44e5410ff7c58074a9090
Rhythm069 <rhythmacharya19@gmail.com>
2026-08-21 10:40:19 +0545

    App setup HeroHealth

--- core/core/settings.py
+++ core/core/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'HeroHealth.apps.HeroHealths.Config',
+    'HeroHealth.apps.HerohealthConfig',
 ]
 
 MIDDLEWARE = [
